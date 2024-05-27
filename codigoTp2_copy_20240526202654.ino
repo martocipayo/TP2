311fddcc6ff9531cb83ce79f7a3e569d1e373841
martocipayo <162316029+martocipayo@users.noreply.github.com>
2024-05-26 22:08:46 -0300

    Update codigoTp2_4.ino

--- codigoTp2_copy_20240526202654.ino
+++ codigoTp2_copy_20240526202654.ino
@@ -1,92 +1,40 @@
 
- void setup()
+void setup()
 {
-  pinMode(0, OUTPUT);
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  pinMode(13, OUTPUT);
-  pinMode(A2, INPUT);
+  pinMode( 10 , OUTPUT );
+  pinMode( 9 , OUTPUT );
+  pinMode( 7 , OUTPUT );
+  pinMode( 5 , OUTPUT );
+  pinMode( 4 , OUTPUT );
+  pinMode( 3 , OUTPUT );
 }
 
 void loop()
 {
-  int parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-
-  digitalWrite(0, HIGH);
-  delay(parpadeo);
-  
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  digitalWrite(1, HIGH);
-  delay(parpadeo);
-  
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  digitalWrite(2, HIGH);
-  delay(parpadeo);
-  
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  analogWrite(3, 140);
-  digitalWrite(4, HIGH);
-  delay(parpadeo);
-  
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  analogWrite(5, 80);
-  analogWrite(6, 200);
-  delay(parpadeo);
-  
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
+  int parpadeo = map(analogRead(A2),0,1023,200,1500);
   digitalWrite(7, HIGH);
-  digitalWrite(8, HIGH);
-  delay(parpadeo);
   
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  digitalWrite(13, HIGH);
-  digitalWrite(12, HIGH);
-  delay(parpadeo);
+  parpadeo(500);
   
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  analogWrite(11, 100);
-  delay(parpadeo);
-  
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  analogWrite(10, 57);
-  delay(parpadeo);
-  
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  analogWrite(9, 90);
-  delay(parpadeo);
-
-  // Apagar todos los LEDs
-  digitalWrite(0, LOW);
-  digitalWrite(1, LOW);
-  digitalWrite(2, LOW);
-  digitalWrite(3, LOW);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
-  digitalWrite(6, LOW);
   digitalWrite(7, LOW);
-  digitalWrite(8, LOW);
-  digitalWrite(9, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
-  digitalWrite(13, LOW);
-
-  parpadeo = map(analogRead(A2), 0, 1023, 0, 1300);
-  delay(parpadeo);
-}
-
-void buzz() {
-  digitalWrite(12, HIGH);
-  delay(100);
-  digitalWrite(12, LOW);
-}
+  analogWrite(9 , 0);
+  analogWrite(10 , 0);
+  analogWrite(3 , 255);
+  
+  parpadeo(1000);
+  
+  digitalWrite(7, HIGH);
+  
+  parpadeo(500);
+  
+  digitalWrite(7, LOW);
+  analogWrite(3 , 0);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  
+  parpadeo(1000);
+  
+  digitalWrite(7, HIGH);
+  
+  parpadeo(100);
+  
